45d6d7a189fbef0d27aff2c2104e09e56f64c825
ChronifyAI <anatoliy@chronifyai.com>
2025-12-26 15:46:32 -0500

    unit test update 8

--- version.php
+++ version.php
@@ -2,8 +2,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_chronifyai';
-$plugin->version = 2025012706;  // CSS linting fixes
+$plugin->version = 2025012707;  // Mustache template fixes
 $plugin->requires = 2022041900;
 $plugin->supported = [400, 403];
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v1.2.6';  // CSS lint clean
+$plugin->release = 'v1.2.7';  // Mustache lint clean
